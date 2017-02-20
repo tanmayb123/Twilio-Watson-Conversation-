17ef8897df8744f2c5f96e418cca75c0a11e7abb
Tanmay Bakshi <tajymany@gmail.com>
2017-02-20 18:44:21 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var ConversationV1 = require('watson-developer-cloud/conversation/v1');
+
+var app = express();
+
+var contexts = [];
+
+app.get('/smssent', function (req, res) {
+  var message = req.query.Body;
+  var number = req.query.From;
+  var twilioNumber = req.query.To;
+
+  var context = null;
+  var index = 0;
+  var contextIndex = 0;
+  contexts.forEach(function(value) {
+    console.log(value.from);
+    if (value.from == number) {
+      context = value.context;
+      contextIndex = index;
+    }
+    index = index + 1;
+  });
+
+  console.log('Recieved message from ' + number + ' saying \'' + message  + '\'');
+
+  var conversation = new ConversationV1({
+    username: '',
+    password: '',
+    version_date: ConversationV1.VERSION_DATE_2016_09_20
+  });
+
+  console.log(JSON.stringify(context));
+  console.log(contexts.length);
+
+  conversation.message({
+    input: { text: message },
+    workspace_id: '',
+    context: context
+   }, function(err, response) {
+       if (err) {
+         console.error(err);
+       } else {
+         console.log(response.output.text[0]);
+         if (context == null) {
+           contexts.push({'from': number, 'context': response.context});
+         } else {
+           contexts[contextIndex].context = response.context;
+         }
+
+         var intent = response.intents[0].intent;
+         console.log(intent);
+         if (intent == "done") {
+           //contexts.splice(contexts.indexOf({'from': number, 'context': response.context}),1);
+           contexts.splice(contextIndex,1);
+           // Call REST API here (order pizza, etc.)
+         }
+
+         var client = require('twilio')(
+           '',
+           ''
+         );
+
+         client.messages.create({
+           from: twilioNumber,
+           to: number,
+           body: response.output.text[0]
+         }, function(err, message) {
+           if(err) {
+             console.error(err.message);
+           }
+         });
+       }
+  });
+
+  res.send('');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
